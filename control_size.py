278bb17ad8e44fc8e5f6d77afe725a5571230026
Roman <90802063+eeeZEGEN@users.noreply.github.com>
2024-07-01 13:32:59 +0300

    Update control_size.py

--- control_size.py
+++ control_size.py
@@ -3,8 +3,8 @@ import threading
 import time
 import json
 
-
 class Controller:
+
     control_objects: list = None
     size: int = None
     main_thr: threading.Thread = None
@@ -14,14 +14,26 @@ class Controller:
         self.size = kwargs.get('size', 0)
         self.main_thr = threading.Thread(target=self.__check_size)
 
+    def __get_size(self, path: str) -> int:
+        total_size = 0
+        if os.path.isfile(path):
+            total_size = os.path.getsize(path)
+        elif os.path.isdir(path):
+            for dirpath, dirnames, filenames in os.walk(path):
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    # Skip if it is symbolic link
+                    if not os.path.islink(fp):
+                        total_size += os.path.getsize(fp)
+        return total_size
+
     def __check_size(self) -> None:
         while True:
             for obj in self.control_objects:
                 try:
                     obj_path = os.path.expanduser(obj)
                     if os.path.exists(obj_path):
-                        file_size_str = os.popen("ls -l %s | awk '{print $5}'" % obj_path).read().strip().replace('0\n', '')
-                        file_size = int(file_size_str)
+                        file_size = self.__get_size(obj_path)
                         if file_size >= self.size:
                             if os.path.isdir(obj_path):
                                 for file in os.listdir(obj_path):
@@ -32,7 +44,6 @@ class Controller:
                             else:
                                 with open(obj_path, 'w') as f:
                                     f.write('')
-
                 except Exception as e:
                     print(f"Error processing {obj}: {str(e)}")
             time.sleep(2)
