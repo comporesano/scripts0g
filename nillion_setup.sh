0c97e9d70f5d3026eae0a0ab8e863f4068507e1c
Roman Bakharev <90802063+comporesano@users.noreply.github.com>
2024-08-15 15:36:26 +0300

    Update nillion_setup.sh

--- nillion_setup.sh
+++ nillion_setup.sh
@@ -16,6 +16,23 @@ nilliond config set client chain-id $CHAIN_ID
 nilliond config set client keyring-backend os
 nilliond config set client node tcp://localhost:${NILLION_PORT}657
 
+sudo rsync -av --remove-source-files .nillionapp/ /home/ritual/.nillionapp/
+
+sudo tee $NIL_TARGET_PATH/.nillionapp/validator.json > /dev/null <<EOF
+{
+        "pubkey": $(nilliond tendermint show-validator),
+        "amount": "unil",
+        "moniker": "$NILLION_MONIK",
+        "identity": "",
+        "website": "",
+        "security": "",
+        "details": "",
+        "commission-rate": "0.1",
+        "commission-max-rate": "0.2",
+        "commission-max-change-rate": "0.01",
+        "min-self-delegation": "1"
+}
+EOF
 rm $NIL_TARGET_PATH/.nillionapp/config/genesis.json $NIL_TARGET_PATH/.nillionapp/config/addrbook.json
 wget -P $NIL_TARGET_PATH/.nillionapp/config http://88.99.208.54:1433/genesis.json
 wget -P $NIL_TARGET_PATH/.nillionapp/config http://88.99.208.54:1433/addrbook.json
@@ -36,19 +53,3 @@ sed -i \
 sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${NILLION_PORT}958\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${NILLION_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${NILLION_PORT}960\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${NILLION_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${NILLION_PORT}966\"%" $NIL_TARGET_PATH/.nillionapp/config/config.toml
 sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:${NILLION_PORT}917\"%; s%^address = \":8080\"%address = \":${NILLION_PORT}980\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:${NILLION_PORT}990\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${NILLION_PORT}991\"%; s%:8545%:${NILLION_PORT}945%; s%:8546%:${NILLION_PORT}946%; s%:6065%:${NILLION_PORT}965%" $NIL_TARGET_PATH/.nillionapp/config/app.toml
 sed -i '/\[rpc\]/,/\[/{s/^laddr = "tcp:\/\/127\.0\.0\.1:/laddr = "tcp:\/\/0.0.0.0:/}' $NIL_TARGET_PATH/.nillionapp/config/config.toml
-
-sudo tee $NIL_TARGET_PATH/.nillionapp/validator.json > /dev/null <<EOF
-{
-        "pubkey": $(nilliond tendermint show-validator),
-        "amount": "unil",
-        "moniker": "$NILLION_MONIK",
-        "identity": "",
-        "website": "",
-        "security": "",
-        "details": "",
-        "commission-rate": "0.1",
-        "commission-max-rate": "0.2",
-        "commission-max-change-rate": "0.01",
-        "min-self-delegation": "1"
-}
-EOF
