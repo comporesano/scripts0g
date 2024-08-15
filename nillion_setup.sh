32e015646b16a41ce7487fcda35c9d6d4c757414
Roman Bakharev <90802063+comporesano@users.noreply.github.com>
2024-08-15 15:47:20 +0300

    Update nillion_setup.sh

--- nillion_setup.sh
+++ nillion_setup.sh
@@ -16,7 +16,7 @@ nilliond config set client chain-id $CHAIN_ID
 nilliond config set client keyring-backend os
 nilliond config set client node tcp://localhost:${NILLION_PORT}657
 
-cp -a /root/.nillionapp/config /home/ritual/.nillionapp
+cp -R /root/.nillionapp/config/ /home/ritual/.nillionapp/config/
 
 sudo tee $NIL_TARGET_PATH/.nillionapp/validator.json > /dev/null <<EOF
 {
